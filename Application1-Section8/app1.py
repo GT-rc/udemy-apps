79cd7fccc831372667c4bd0ef2489d549b3ae151
Glynnis Thomas <glynnisthomas@gmail.com>
2017-11-23 02:28:33 -0600

    Fixed proper noun bug

--- Application1-Section8/app1.py
+++ Application1-Section8/app1.py
@@ -1,9 +1,10 @@
 """
-Console program to search for word definitions. Checks if word is in the included json file and makes suggestions for common misspellings.
+Console program to search for word definitions. Checks if word is in the included json file and makes suggestions 
+for common misspellings.
 """
 
 import json
-from difflib import SequenceMatcher, get_close_matches
+from difflib import get_close_matches
 
 f = "./data.json"
 data = json.load(open(f, 'r'))
@@ -11,11 +12,9 @@ data = json.load(open(f, 'r'))
 def in_dict(word):
     """Checks if the word is in the dict, and returns def if it is."""
     if word in data:
-        defin = data[word]
-        if len(defin) > 1:
-            return '\n'.join(defin)
-        else:
-            return defin[0]
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
     elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
         response = input("Did you mean %s? Enter [Y] if yes, and [N] if no: " % get_close_matches(word, data.keys())[0])
         if response.lower() == 'y':
@@ -29,8 +28,13 @@ def in_dict(word):
 
 
 def main():
+    """Main program"""
     user_word = input("Enter a word: ").lower()
-    print(in_dict(user_word))
+    output = in_dict(user_word)
+    if type(output) == list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
 
-if (__name__) == (__main__):
-    main()
+main()
