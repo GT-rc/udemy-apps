b664ae495a76892c633bd4692274dddc993a3a51
Glynnis Thomas <glynnisthomas@gmail.com>
2017-11-23 01:20:06 -0600

    Completed program and tested

--- Application1-Section8/app1.py
+++ Application1-Section8/app1.py
@@ -1,4 +1,6 @@
-# TODO 1: create a function to search the dict for a key and print out the output
+"""
+Console program to search for word definitions. Checks if word is in the included json file and makes suggestions for common misspellings.
+"""
 
 import json
 from difflib import SequenceMatcher, get_close_matches
@@ -9,14 +11,26 @@ data = json.load(open(f, 'r'))
 def in_dict(word):
     """Checks if the word is in the dict, and returns def if it is."""
     if word in data:
-        return data[word]
-    elif get_close_matches(word, data.keys()) != None:
-        return "Did you mean " + get_close_matches(word, data.keys())[0] + "?"
+        defin = data[word]
+        if len(defin) > 1:
+            return '\n'.join(defin)
+        else:
+            return defin[0]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        response = input("Did you mean %s? Enter [Y] if yes, and [N] if no: " % get_close_matches(word, data.keys())[0])
+        if response.lower() == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        elif response.lower() == 'n':
+            return "That word is not in my dictionary. Please try a different word or another resource."
+        else:
+            return "I didn't understand that entry."
     else:
         return "That word is not in my dictionary. Please try a different word or another resource."
 
-word = input("Enter a word: ").lower()
 
-print(in_dict(word))
+def main():
+    user_word = input("Enter a word: ").lower()
+    print(in_dict(user_word))
 
-
+if (__name__) == (__main__):
+    main()
